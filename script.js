e70b72a9c88c1ea8431f843a91e53d23c1b0b2f0
pepel7 <dasha.per.2016@gmail.com>
2023-12-01 18:59:34 +0200

    Add keyboard support and fix 2 bugs
    The fixed bugs:
    
     - The symbol "Approximately equal" in an inappropriate situation, when in fact the result is exactly equal.
    
     - When you haven't input anything yet, you can press zero and there will be a lot of zeros.

--- script.js
+++ script.js
@@ -17,10 +17,14 @@ function divide(num1, num2) {
 let num1 = 0;
 let num2 = 0;
 let operatorValue = null;
-let displayValue = '';
+let displayValue = '0';
 let resultValue = null;
 
 let decimalSavingValue;
+
+function findApproximatelyOrNot(resultValue) {
+    return resultValue.toString().split('.').pop().length > 2;
+};
 
 function operate(num1, operatorValue, num2) {
     if(operatorValue == '÷' && num2 == 0) return resultValue = "You can't divide by zero~";
@@ -28,40 +32,48 @@ function operate(num1, operatorValue, num2) {
     if(operatorValue == null && num2 == 0 ) return resultValue = num1;
     switch (operatorValue) {
         case '+':
-            resultValue = add(num1, num2)
+            resultValue = add(num1, num2);
             if(resultValue.toString().split('').includes('.')) {
-                decimalSavingValue = Math.round(resultValue * 100) / 100;
-                resultValue = `≈${decimalSavingValue}`;
+                if(findApproximatelyOrNot(resultValue)) {
+                    decimalSavingValue = Math.round(resultValue * 100) / 100;
+                    resultValue = `≈${decimalSavingValue}`;
+                };
             }
             break;
         case '-':
-            resultValue = substract(num1, num2)
+            resultValue = substract(num1, num2);
             if(resultValue.toString().split('').includes('.')) {
-                decimalSavingValue = Math.round(resultValue * 100) / 100;
-                resultValue = `≈${decimalSavingValue}`;
+                if(findApproximatelyOrNot(resultValue)) {
+                    decimalSavingValue = Math.round(resultValue * 100) / 100;
+                    resultValue = `≈${decimalSavingValue}`;
+                };
             }
             break;
         case '*':
-            resultValue = multiply(num1, num2)
+            resultValue = multiply(num1, num2);
             if(resultValue.toString().split('').includes('.')) {
-                decimalSavingValue = Math.round(resultValue * 100) / 100;
-                resultValue = `≈${decimalSavingValue}`;
+                if(findApproximatelyOrNot(resultValue)) {
+                    decimalSavingValue = Math.round(resultValue * 100) / 100;
+                    resultValue = `≈${decimalSavingValue}`;
+                };
             }
             break;
         case '÷':
-            resultValue = divide(num1, num2)
+            resultValue = divide(num1, num2);
             if(resultValue.toString().split('').includes('.')) {
-                decimalSavingValue = Math.round(resultValue * 100) / 100;
-                resultValue = `≈${decimalSavingValue}`;
+                if(findApproximatelyOrNot(resultValue)) {
+                    decimalSavingValue = Math.round(resultValue * 100) / 100;
+                    resultValue = `≈${decimalSavingValue}`;
+                };
             }
             break;
     };
 };
 
 const outputField = document.querySelector('.display-text');
+outputField.textContent = '0';
 
 function populateDisplay(valueToPrint) {
-    if(num1 == 0 && !displayValue) displayValue = 0;
     if(valueToPrint == '÷' || valueToPrint == '*' || valueToPrint == '-' || valueToPrint == '+') {
         if(!operatorValue) displayValue += valueToPrint;
         else {
@@ -73,7 +85,7 @@ function populateDisplay(valueToPrint) {
             displayValue += valueToPrint;
         }
     } else {
-        if(!displayValue) displayValue = valueToPrint;
+        if(displayValue == '0') displayValue = valueToPrint;
         else displayValue += valueToPrint;
     } 
     outputField.textContent = `${displayValue}`;
@@ -88,8 +100,9 @@ let clickEvent = new Event('click');
 for(let digit of digits) {
     digit.addEventListener('click', () => {
         if(!operatorValue) {
-            populateDisplay(digit.textContent);
             num1 = !num1 ? digit.textContent : num1 + digit.textContent;
+            populateDisplay(digit.textContent);
+            
         } else {
             populateDisplay(digit.textContent)
             num2 = !num2 ? digit.textContent : num2 + digit.textContent;
@@ -108,9 +121,9 @@ for(let operator of operators) {
     });
 };
 
-equalButton.addEventListener('click', () => {
+function pressEqualButton() {
     operate(+num1, operatorValue, +num2);
-    displayValue = '';
+    displayValue = '0';
     populateDisplay(resultValue);
     num1 = decimalSavingValue ? decimalSavingValue : 
            resultValue == "You can't divide by zero~" ? 0 : resultValue;
@@ -119,21 +132,25 @@ equalButton.addEventListener('click', () => {
     if(!num1.toString().split('').includes('.')) dotBtn.disabled = false;
     else dotBtn.disabled = true;
     resultValue = null;
-});
+}
+
+equalButton.addEventListener('click', pressEqualButton);
 
 const backspaceBtn = document.querySelector('.backspace');
 
-backspaceBtn.addEventListener('click', () => {
+function pressBackspaceButton() {
     if(!operatorValue) {
-        num1 = num1.substring(0, num1.length - 1);
+        num1 = num1.toString().substring(0, num1.length - 1);
     } else if (operatorValue && !num2) {
         operatorValue = operatorValue.substring(0, operatorValue.length - 1);
     } else {
-        num2 = num2.substring(0, num2.length - 1);
+        num2 = num2.toString().substring(0, num2.length - 1);
     };
-    displayValue = displayValue.substring(0, displayValue.length - 1);
+    displayValue = displayValue.toString().substring(0, displayValue.length - 1);
     populateDisplay('');
-});
+}
+
+backspaceBtn.addEventListener('click', pressBackspaceButton);
 
 const clearElementBtn = document.querySelector('.clear-element');
 
@@ -164,10 +181,52 @@ clearAllBtn.addEventListener('click', () => {
 const dotBtn = document.querySelector('.dot');
 
 dotBtn.addEventListener('click', () => {
-    if (num2) num2 += '.';
-    else if (num1 && !operatorValue) {
+    if (operatorValue) num2 += '.';
+    else if (!operatorValue) {
         num1 += '.';
     } else return;
     populateDisplay('.');
     dotBtn.disabled = true;
+});
+
+//---------------------
+//      Keyboard
+//---------------------
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key == '0' && num1 == 0) return;
+    if(event.key.match(/[^0-9]/g)) return;
+    if(!operatorValue) {
+        populateDisplay(event.key);
+        num1 = !num1 ? event.key : num1 + event.key;
+    } else {
+        populateDisplay(event.key)
+        num2 = !num2 ? event.key : num2 + event.key;
+    };
+
+    if( operatorValue && !(num2.toString().split('').includes('.')) ) {
+        dotBtn.disabled = false;
+    };
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key.match(/[+*/-]/g)) {
+        if(event.key == '/') {
+            populateDisplay('÷');
+            operatorValue = '÷';
+            return
+        }
+        populateDisplay(event.key);
+        operatorValue = event.key;
+    }
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key != 'Backspace') return;
+    pressEqualButton();
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key != 'Backspace' || event.key != 'Delete') return;
+    pressBackspaceButton();
 });
